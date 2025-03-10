ee24a462a111209a0fb79337af964e053edbc0ed
Sophie <sophtompa@gmail.com>
2025-03-10 15:03:00 +0000

    add 2-get-all-topics and general error handling

---
+++ controllers/errors.controller.js
@@ -0,0 +1,21 @@
+const handlePsqlError = (err, req, res, next) => {
+    if(err.code === '22P02') {
+        res.status(400).send({msg: 'bad request'})
+    }
+    next(err);
+};
+
+const handleCustomError = (err, req, res, next) => {
+    if(err.status) {
+        res.status(err.status).send({msg: err.msg})
+    }
+    next(err);
+}
+
+
+
+const handleServerError = (err, req, res, next) => {
+    res.status(500).send({msg: 'internal server error'})
+}
+
+module.exports = { handlePsqlError, handleCustomError, handleServerError }
--- models/nc-news.models.js
+++ models/nc-news.models.js
@@ -1,2 +1,12 @@
 const db = require('../db/connection.js');
 const format = require('pg-format');
+
+const fetchTopics = () => {
+    return db.query(`SELECT * FROM topics`)
+    .then(({rows}) => {
+        console.log(rows)
+        return rows;
+    })
+};
+
+module.exports = { fetchTopics }
