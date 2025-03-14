95c8c551d23eba6d36d2ff1b5793f46857d9a1a3
Sophie <sophtompa@gmail.com>
2025-03-14 16:29:51 +0000

    add changes

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,6 @@ const ENV = process.env.NODE_ENV || 'dev'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set")
 } else { 
